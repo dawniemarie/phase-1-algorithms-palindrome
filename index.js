11e379cd52b45ade0e14091f737cc00923b9e0ed
Dawn Olivier <dmarieolivier@gmail.com>
2022-05-30 13:12:03 -0400

    commit tree 27912f40cca32707728cd9803e032efe5184614d

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const length = word.length
+  for (let i=0; i<length / 2; i++) {
+    if (word[i] !== word[length-1-i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+Find the string length and loop through half of the string.
+If the first and last string are not the same, return false.
+If the first and last string are the same, return true.
 */
 
 /*
@@ -12,6 +20,10 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
+  console.log(isPalindrome("reviver"))
+  console.log(isPalindrome("level"))
+  console.log(isPalindrome("cat"))
+  console.log(isPalindrome("dog"))
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
